3d9926ac2d3eacf338e1e29fa2cd0a65c16ab2b4
JadnarRoverdin <jadnarroverdin@gmail.com>
2017-11-28 08:29:13 -0500

    e

--- models/email.php
+++ models/email.php
@@ -1,10 +1,11 @@
 <?php
 class Email
 {
-  $to = "jadnarroverdin@gmail.com";
-  
+
+
   public send($fromemail, $fromname,$text)
   {
+    $to = "jadnarroverdin@gmail.com";
     $subject = 'HT-CONTACT from '.$fromname;
     $headers = 'From: contact@huskehtech.com' . "\r\n" . 'Reply-To: no-reply@huskehtech.com';
     $message = $fromname." used Huskeh Tech's contact form \n".
