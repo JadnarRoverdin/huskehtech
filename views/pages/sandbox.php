124088019746fa84923ba1f67a1f4eaa83b719d0
JadnarRoverdin <jadnarroverdin@gmail.com>
2017-12-05 08:08:40 -0500

    added explanation for sandbox

--- views/pages/sandbox.php
+++ views/pages/sandbox.php
@@ -1,4 +1,8 @@
 <h2>Sandbox</h2>
+<div> This page is here to display current works.
+  The code executed here is potentially unstable and may not return meaningful results.
+  This page is subject to change.
+</div>
 
 
 <?php
