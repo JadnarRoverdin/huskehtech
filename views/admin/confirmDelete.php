b6c94106f2d0b7b61d21ee2c65db5fe8bad7cff7
JadnarRoverdin <jadnarroverdin@gmail.com>
2018-01-23 18:13:48 -0500

    e

--- views/admin/confirmDelete.php
+++ views/admin/confirmDelete.php
@@ -3,7 +3,7 @@ Deletion Confirmation
 Are you sure you want to delete post titled <?php echo $postName; ?>?
 
 <form action='?controller=admin&action=deletePost' method='post'>
-  <input class='input' type='hidden' name='postID[]' value='<?php echo $postID; ?>'>
+  <input class='input' type='hidden' name='postID' value='<?php echo $postID; ?>'>
   <select class='input' id ='scroller' name='yesno'>
     <option value ='no'>No</option>
     <option value ='yes'>Yes</option>
